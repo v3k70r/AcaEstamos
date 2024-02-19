8c1f7b603cf6a98271b26d710cadbeae9566af3d
Kathb <kathy.belardy@gmail.com>
2024-02-19 11:43:34 -0300

    avances 4

--- FRONTEND/src/Components/PerfilTalentos/CardTalentosFormacion/CardTalentosFormacion.jsx
+++ FRONTEND/src/Components/PerfilTalentos/CardTalentosFormacion/CardTalentosFormacion.jsx
@@ -1,17 +1,19 @@
 import './CardTalentoFormacion.css';
 
 function CardTalentoFormacion(props) {
+    // eslint-disable-next-line react/prop-types
+    const  {institucion, academicas, fecha} = props  
     return (
         <div className="card cardF2">
             <div className="card-body cardbodyF2 d-flex ">
                 <div className="mx-auto text-center"> 
                     <i className="fas fa-graduation-cap fa-3x" style={{ marginRight: '25px', marginBottom: '20px' }} ></i> 
-                    <h6 className="card-title">{props.institucion}</h6>
+                    <h6 className="card-title">{institucion}</h6>
                     <hr />
                 </div>
                 <div className="contiene" style={{ width: '100%' }}>
-                    <p className="card-text">{props.academicas}</p>
-                    <p>{props.fecha}</p>
+                    <p className="card-text">{academicas}</p>
+                    <p>{fecha}</p>
                 </div>
             </div>
         </div>
