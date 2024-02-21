62e15e6128a220e3d79fe56f2e89a41fee1fd6f1
ximenagay <xipazgay@gmail.com>
2024-02-20 22:14:44 -0300

    rutas

--- FRONTEND/src/Router/routes.jsx
+++ FRONTEND/src/Router/routes.jsx
@@ -5,10 +5,7 @@ import EnrolamientoTalent from "../Views/EnrolamientoTalent";
 import InicioDeSesion from "../Views/InicioDeSesion";
 import VistaDeEmpresas from "../Views/VistaDeEmpresas";
 import IngresoUsuario from "../Views/IngresoUsuario";
-
 import PerfilTalentos from "../Views/PerfilTalentos";
-
-
 import AcercaDe from "../Views/AcercaDe";
 import PerfilEmpresa from "../Views/PerfilEmpresa"
 import EnrolamientoEmpresa from "../Views/EnrolamientoEmpresa";
@@ -21,11 +18,7 @@ const router = createBrowserRouter([
   { path: "/inicioDeSesion", element: <InicioDeSesion />},
   {path: "/ingresoUsuario", element: <IngresoUsuario />},
   {path: "/VistaDeEmpresas", element: <VistaDeEmpresas/>},
-
-  {path: "/PerfilTalentos", element: <PerfilTalentos />}
-    
-]);
-
+  {path: "/PerfilTalentos", element: <PerfilTalentos />},
   {path: "/AcercaDe", element: <AcercaDe />},
   { path: "/PerfilEmpresa", element: <PerfilEmpresa />},
   { path: "/EnrolamientoEmpresa", element: <EnrolamientoEmpresa />},
