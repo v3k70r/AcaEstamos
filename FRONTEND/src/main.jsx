15f3cda4024fd29edeb97cc4a410cba110fe98a7
Víctor Albarrán <victor.a.albarran.n@gmail.com>
2024-02-22 17:20:45 -0300

    Intalacion de poppins archivoblack y emoticones

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router/routes';
-
+import './all.css'
 
 
 
