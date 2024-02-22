f3d59003e72dbcc2dd3b6fe650ba72b0e7ed16e4
“Daniela <danielacrmarquez@gmail.com>
2024-02-22 19:54:26 -0300

    menuadmin

--- FRONTEND/src/Components/VistaAdministrador/CarruselCVAdministrador/CarruselCVAdministrador.jsx
+++ FRONTEND/src/Components/VistaAdministrador/CarruselCVAdministrador/CarruselCVAdministrador.jsx
@@ -1,31 +1,86 @@
-import './CarruselCvAdministrador.css';
+import { useEffect, useRef } from 'react';
+import Flickity from 'flickity';
 
-function CarruselCvAdministrador(props) {
-    // eslint-disable-next-line react/prop-types
-    const {nombreCarrusel, interesCarrusel, imagenCarrusel} = props
+function CarruselCVAdministrador() {
+  const flickityRef = useRef(null);
+
+  useEffect(() => {
+    flickityRef.current = new Flickity('.kathcarrusel', {
+      wrapAround: true,
+      autoPlay: true,
+      imagesLoaded: true
+    });
+
+    return () => {
+      flickityRef.current.destroy();
+    };
+  }, []);
+
   return (
-    <div className="carousel-cell">
-      <li className="card carruselcard">
-      <div className="img imgcarrusel"><img src={imagenCarrusel} alt="img" draggable="false" width="60%" /></div>
-        <h2>{nombreCarrusel}</h2>
-        <span>{interesCarrusel}</span>
-        <button className="boton-tarjeta">Ver Talento</button>
-      </li>
+    <>
+        <h4 className="h3-carrusel text-center m-4" style={{ backgroundColor: '#E5ECF6', borderRadius: '5px' }}>Curriculums</h4>
+        <div className="ContenedorCarrusel">
+          <h1 className="w3-center"></h1>
+          <div className="carousel kathcarrusel">
+            <div className="carousel-cell">
+              <li className="card carruselcard">
+                <div className="img imgcarrusel"><img src="images/cv-admin.png" alt="img" draggable="false" width="60%" /></div>
+                <h2>Nombre Talento</h2>
+                <span>Curriculum</span>
+                <button className="boton-tarjeta" style={{ backgroundColor: '#769E33' }}>Aprobar</button>
+                <button className="boton-tarjeta m-3" style={{ backgroundColor: '#e33131' }}>Denegar</button>
+              </li>
+            </div>
+            {/* Los otros elementos del carrusel */}
+            <div className="carousel-cell">
+        <li className="card carruselcard">
+          <div className="img imgcarrusel"><img src="images/cv-admin.png" alt="img" draggable="false" width="60%" /></div>
+          <h2>Nombre Talento</h2>
+          <span>Curriculum</span>
+          <button className="boton-tarjeta" style={{ backgroundColor: '#769E33'}}>Aprobar</button>
+          <button className="boton-tarjeta m-3" style={{ backgroundColor: '#e33131'}}>Denegar</button>
+        </li>
+      </div>
+      <div className="carousel-cell">
+        <li className="card carruselcard">
+          <div className="img imgcarrusel"><img src="images/cv-admin.png" alt="img" draggable="false"width="60%" /></div>
+          <h2>Nombre Talento</h2>
+          <span>Curriculum</span>
+          <button className="boton-tarjeta" style={{ backgroundColor: '#769E33'}}>Aprobar</button>
+          <button className="boton-tarjeta m-3" style={{ backgroundColor: '#e33131'}}>Denegar</button>
+        </li>
+      </div>
+      <div className="carousel-cell">
+        <li className="card carruselcard">
+          <div className="img imgcarrusel"><img src="images/cv-admin.png" alt="img" draggable="false"width="60%" /></div>
+          <h2>Nombre Talento</h2>
+          <span>Curriculum</span>
+          <button className="boton-tarjeta" style={{ backgroundColor: '#769E33'}}>Aprobar</button>
+          <button className="boton-tarjeta m-3" style={{ backgroundColor: '#e33131'}}>Eliminar</button>
+        </li>
+      </div>
+      <div className="carousel-cell">
+        <li className="card carruselcard">
+          <div className="img imgcarrusel"><img src="images/cv-admin.png" alt="img" draggable="false"width="60%" /></div>
+          <h2>Nombre Talento</h2>
+          <span>Curriculum</span>
+          <button className="boton-tarjeta" style={{ backgroundColor: '#769E33'}}>Aprobar</button>
+          <button className="boton-tarjeta m-3" style={{ backgroundColor: '#e33131'}}>Eliminar</button>
+        </li>
+      </div>
+      <div className="carousel-cell">
+        <li className="card carruselcard">
+          <div className="img imgcarrusel"><img src="images/cv-admin.png" alt="img" draggable="false"width="60%" /></div>
+          <h2>Nombre Talento</h2>
+          <span>Curriculum</span>
+          <button className="boton-tarjeta" style={{ backgroundColor: '#769E33'}}>Aprobar</button>
+          <button className="boton-tarjeta m-3" style={{ backgroundColor: '#e33131'}}>Eliminar</button>
+        </li>
+      </div>
     </div>
+    </div>
+    </>
   );
 }
 
-{/* <h4 className="h3-carrusel text-center m-4" style={{ backgroundColor: '#E5ECF6', borderRadius: '5px' }}>Curriculums</h4>
-<div className="ContenedorCarrusel">
-  <h1 className="w3-center"></h1>
-  <div className="carousel kathcarrusel">
-    <div className="carousel-cell">
-      <li className="card carruselcard">
-        <div className="img imgcarrusel"><img src="images/cv-admin.png" alt="img" draggable="false" width="60%" /></div>
-        <h2>Nombre Talento</h2>
-        <span>Curriculum</span>
-        <button className="boton-tarjeta" style={{ backgroundColor: '#769E33' }}>Aprobar</button>
-        <button className="boton-tarjeta m-3" style={{ backgroundColor: '#e33131' }}>Denegar</button>
-      </li>
-</div> */}
-export default  CarruselCvAdministrador;
+export default CarruselCVAdministrador;
--- FRONTEND/src/Views/VistaAdministrador.jsx
+++ FRONTEND/src/Views/VistaAdministrador.jsx
@@ -1,58 +1,26 @@
 import Navbar from '../Components/Navbar/Navbar'
 import BannerAdministrador from '../Components/VistaAdministrador/BannerAdministrador/BannerAdministrador'
 import TextoIntroAdministrador from '../Components/VistaAdministrador/TextoIntroAdministrador/TextoIntroAdministrador'
+import MenuAdministrador from '../Components/VistaAdministrador/MenuAdministrador/MenuAdministrador'
 import CardsHorizontalAdministrador from '../Components/VistaAdministrador/CardsHorizontalAdministrador/CardsHorizontalAdministrador'
 import CarruselPerfilesAdministrador from '../Components/VistaAdministrador/CarruselPerfilesAdministrador/CarruselPerfilesAdministrador';
-import CarruselCVAdministrador from '../Components/VistaAdministrador/CarruselCvAdministrador/CarruselCvAdministrador';
+import CarruselCvAdministrador from '../Components/VistaAdministrador/CarruselCvAdministrador/CarruselCvAdministrador';
 import Footer from '../Components/Footer/Footer';
 
 function VistaAdministrador() {
   return (
-    <>
+    <div>
     <Navbar />
     <BannerAdministrador />
     <TextoIntroAdministrador />
+    <MenuAdministrador />
     <CardsHorizontalAdministrador />
+    <div className="carousel kathcarrusel"  data-flickity='{ "wrapAround": true, "autoPlay": true, "imagesLoaded":true }'>
     <CarruselPerfilesAdministrador />
-  
-    <h3 className="h3-carrusel text-center" style={{ color: '#424242' }}>perfiles relacionados</h3>
-  <div className="ContenedorCarrusel">
-  <h1 className="w3-center" ></h1>
-  <div className="carousel kathcarrusel"  data-flickity='{ "wrapAround": true, "autoPlay": true, "imagesLoaded":true }'>
-<CarruselCvAdministrador 
-nombreCarrusel='Daniel Cansino'
-interesCarrusel='Informática'
-imagenCarrusel='public/images/Hombre_avatar1.png'
-/>
-<CarruselCvAdministrador
-nombreCarrusel='Daniel Cansino'
-interesCarrusel='Informática'
-imagenCarrusel='public/images/Hombre_avatar2.png'
-/>
-<CarruselCvAdministrador 
-nombreCarrusel='Daniel Cansino'
-interesCarrusel='Informática'
-imagenCarrusel='public/images/Hombre_avatar3.png'
-/>
-<CarruselCvAdministrador
-nombreCarrusel='Daniel Cansino'
-interesCarrusel='Informática'
-imagenCarrusel='public/images/Hombre_avatar4.png'
-/>
-<CarruselCvAdministrador
-nombreCarrusel='Daniel Cansino'
-interesCarrusel='Informática'
-imagenCarrusel='public/images/Hombre_avatar5.png'
-/>
-<CarruselCvAdministrador
-nombreCarrusel='Daniel Cansino'
-interesCarrusel='Informática'
-imagenCarrusel='public/images/Hombre_avatar6.png'
-/>
-  </div>
-  </div>
+    <CarruselCvAdministrador />
+    </div>
     <Footer />
-    </>
+    </div>
   )
 }
 
