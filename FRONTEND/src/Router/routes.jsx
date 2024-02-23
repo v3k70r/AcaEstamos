5985e670250c9ba68200b148206a62d86f51cc8b
ximenagay <xipazgay@gmail.com>
2024-02-23 10:22:16 -0300

    ayuda

--- FRONTEND/src/Router/routes.jsx
+++ FRONTEND/src/Router/routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Home from "../Views/Home";
-import enrolamientoTalent from "../Views/enrolamientoTalent";
+import EnrolamientoTalent from "../Views/enrolamientoTalent";
 import InicioDeSesion from "../Views/InicioDeSesion";
 import VistaDeEmpresas from "../Views/VistaDeEmpresas";
 import IngresoUsuario from "../Views/IngresoUsuario";
@@ -16,7 +16,7 @@ import TableroTalentos from "../Views/TableroTalentos";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home/>},
-  { path: "/enrolamientoTalent", element: <enrolamientoTalent />},
+  { path: "/EnrolamientoTalent", element: <EnrolamientoTalent />},
   { path: "/inicioDeSesion", element: <InicioDeSesion />},
   {path: "/ingresoUsuario", element: <IngresoUsuario />},
   {path: "/VistaDeEmpresas", element: <VistaDeEmpresas/>},
