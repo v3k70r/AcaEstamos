d0c1f9e23dfae38b96835784a4774f72b09a16ed
“Daniela <danielacrmarquez@gmail.com>
2024-02-28 17:04:17 -0300

    media

--- FRONTEND/src/Components/Inicio/BannerBienvenidos/BannerBienvenidos.jsx
+++ FRONTEND/src/Components/Inicio/BannerBienvenidos/BannerBienvenidos.jsx
@@ -1,50 +1,50 @@
-
+import './BannerBienvenidos.css';
 
 function BannerBienvenidos() {
   return (
     //carrusel 
-    <div className="container-fluid">
-    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-      <div id="carouselExampleDark" className="carousel carousel-dark slide">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div className="carousel-inner">
-        <div className="carousel-item active" data-bs-interval="3000">
-          <img src="images/cerro-renca1.png" className="d-block w-100 mx-0" alt="banner 1" />
-          <div className="carousel-caption d-none d-md-block">
-            <h1 style={{fontSize: "100px"}}>¡Bienvenidos!</h1>
-            <p className="text-light" style={{fontSize: "25px"}}>Conectando a talentos con empresas comprometidas por Renca.</p>
-          </div>
-          </div>
-          <div className="carousel-item" data-bs-interval="2000">
-            <img src="images/cerro-renca2.png" className="d-block w-100 mx-0" alt="banner 2" />
-            <div className="carousel-caption d-none d-md-block">
-            <h1 style={{fontSize: "100px"}}>¡Bienvenidos!</h1>
-              <p className="text-light" style={{fontSize: "25px"}}>Conectando a talentos con empresas comprometidas por Renca.</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src="images/cerroRenca1.png" className="d-block w-100 mx-0" alt="banner 3" />
-            <div className="carousel-caption d-none d-md-block">
-            <h1 style={{fontSize: "100px"}}>¡Bienvenidos!</h1>
-              <p className="text-light" style={{fontSize: "25px"}}>Conectando a talentos con empresas comprometidas por Renca.</p>
-            </div>
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+  <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleDark" className="carousel carousel-dark slide">
+      <div className="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active" data-bs-interval="3000">
+          <img src="images/cerro-renca1.png" className="img-fluid mx-auto d-block" alt="banner 1" />
+          <div className="carousel-caption d-none d-md-block">
+            <h1 className="izquierda">¡Bienvenidos!</h1>
+            <h3 className="text-light contenedor">Conectando a talentos con empresas comprometidas por Renca.</h3>
+          </div>
+        </div>
+        <div className="carousel-item" data-bs-interval="2000">
+          <img src="images/cerro-renca2.png" className="img-fluid mx-auto d-block" alt="banner 2" />
+          <div className="carousel-caption d-none d-md-block">
+            <h1 className="izquierda">¡Bienvenidos!</h1>
+            <p className="text-light contenedor">Conectando a talentos con empresas comprometidas por Renca.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="images/cerroRenca1.png" className="img-fluid mx-auto d-block" alt="banner 3" />
+          <div className="carousel-caption d-none d-md-block">
+            <h1 className="izquierda">¡Bienvenidos!</h1>
+            <p className="text-light contenedor">Conectando a talentos con empresas comprometidas por Renca.</p>
+          </div>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
-    </div>
+  </div>
+
+
     //carrusel
   )
 }
