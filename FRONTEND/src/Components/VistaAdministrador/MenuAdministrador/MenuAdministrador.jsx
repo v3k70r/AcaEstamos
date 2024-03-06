81a6406c3cc6c60e5ac8967f7c7cd7c38b11f6fc
franciscovega <fran.vgasalv@gmail.com>
2024-03-05 21:08:42 -0300

    hola

--- FRONTEND/src/Components/VistaAdministrador/MenuAdministrador/MenuAdministrador.jsx
+++ FRONTEND/src/Components/VistaAdministrador/MenuAdministrador/MenuAdministrador.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-const url = 'http://localhost:8080/api/v1/listar'
+import { useEffect, useState } from 'react'
 import './MenuAdministrador.css';
 
+const MenuAdministrador = () => {
+  const [listar, setListar] = useState([])
 
-function MenuAdministrador() {
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/listar')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        //debugger
+        setListar(data.data)})
+  }, [listar])
+
   return (
     <div>
       <div className="col-md-12">
@@ -20,31 +30,26 @@ function MenuAdministrador() {
               <table className="table">
                 <thead>
                   <tr>
-                    <th scope="col" style={{ backgroundColor: '#E5ECF6' }}>Perfiles</th>
-                    <th scope="col" style={{ backgroundColor: '#E5ECF6' }}>Nombre</th>
-                    <th scope="col" style={{ backgroundColor: '#E5ECF6' }}>Apellido paterno</th>
+                    <th scope="col" style={{ backgroundColor: '#E5ECF6' }}>Enumeración</th>
+                    <th scope="col" style={{ backgroundColor: '#E5ECF6' }}>Email</th>
+                    <th scope="col" style={{ backgroundColor: '#E5ECF6' }}>Rol</th>
                     <th scope="col" style={{ backgroundColor: '#E5ECF6' }}>Estado</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td><button className="boton-tarjeta boton-admin" style={{ backgroundColor: '#769E33' }}>Activo</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thor</td>
-                    <td><button className="boton-tarjeta boton-admin" style={{ backgroundColor: '#769E33' }}>Activo</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td><button className="boton-tarjeta boton-admin" style={{ backgroundColor: '#e33131' }}>Inactivo</button></td>
-                  </tr>
+                  {listar.map((usuario) => (
+                    <tr key={usuario._id}>
+                      <th scope="row">{usuario._id}</th>
+                      <td>{usuario.email}</td>
+                      <td>{usuario.status}</td>
+                      <td>{usuario.rol}</td>
+                      <td>
+                        <button className={`boton-tarjeta boton-admin ${usuario.status === 'Activo' ? 'boton-activo' : 'boton-inactivo'}`}>
+                          {usuario.status}
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
