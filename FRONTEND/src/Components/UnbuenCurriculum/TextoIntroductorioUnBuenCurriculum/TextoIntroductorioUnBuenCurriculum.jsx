7357a30b7b857c8db222d13e945b1b4224979a30
KathB165 <kathy.belardy@gmail.com>
2024-03-06 15:23:21 -0300

    res2

--- FRONTEND/src/Components/UnbuenCurriculum/TextoIntroductorioUnBuenCurriculum/TextoIntroductorioUnBuenCurriculum.jsx
+++ FRONTEND/src/Components/UnbuenCurriculum/TextoIntroductorioUnBuenCurriculum/TextoIntroductorioUnBuenCurriculum.jsx
@@ -4,7 +4,7 @@ function TextoIntroductorioUnBuenCurriculum() {
   return (
     <>
     <div>
-      <div id="contenedor-buencv" className="container-sm m-4">
+      <div id="contenedor-buencv" className="container-sm">
       <h4>En nuestra página de consejos para elaborar un buen currículum, encontrarás una valiosa colección de sugerencias para destacarte en el proceso de búsqueda de empleo. Nuestra meta es ayudarte a presentar la mejor versión de ti mismo, para que puedas alcanzar tus objetivos profesionales.</h4>
     </div>
     </div>
