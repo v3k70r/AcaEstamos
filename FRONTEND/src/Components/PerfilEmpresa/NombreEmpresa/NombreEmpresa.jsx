d7af4c7db4e6763346bf9fe4f6a4ec3baa54c796
ximenagay <xipazgay@gmail.com>
2024-03-07 01:39:05 -0300

    planb

--- FRONTEND/src/Components/PerfilEmpresa/NombreEmpresa/NombreEmpresa.jsx
+++ FRONTEND/src/Components/PerfilEmpresa/NombreEmpresa/NombreEmpresa.jsx
@@ -4,8 +4,8 @@ const NombreEmpresa = () => {
             <div className="container mt-3">
                 <div className="row flex-sm-row">
                     <div className="col-lg-9 shadow mt-3" style={{ backgroundColor: '#E5ECF6', borderRadius: '20px' }}>
-                        <h3 className="text-center mt-5" id="empresaName">Nombre de la empresa</h3>
-                        <p className="text-center">Mensaje de la empresa hacia los talentos</p>
+                        <h3 className="text-center mt-5" id="empresaName">Coca-Cola Andina</h3>
+                        <p className="text-center">Llegar juntos a todos los rincones</p>
                     </div>
                     {/* Tarjeta de logo */}
                     <div className="col-lg-3 mt-3">
